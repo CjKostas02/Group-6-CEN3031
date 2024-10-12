9012822f20d7af2fdaec832d6a059ab8be12c6db
CjKostas02 <CjKostas02@gmail.com>
2024-10-11 22:47:20 -0400

    Added class for text boxes. Added function to load buttons and text boxes into data structures and load fonts when application starts.

--- application.cpp
+++ application.cpp
@@ -1,25 +1,59 @@
+#include <iostream>
 #include "application.h"
 #include <map>
 
-void Application::updateLoginWindow(){
+void Application::renderLoginWindow(){
+    // title text
     sf::Text title;
-    title.setFont(font);
+    title.setFont(titleFont);
     title.setFillColor({98, 115, 255});
     title.setString("Campus Central Food");
     title.setPosition(175, 50);
-    title.setScale(2,2);
+    title.setCharacterSize(65);
+
+    // create account hitbox
+    sf::RectangleShape createAccRect({140,25});
+    //createAccRect.setFillColor(sf::Color::White);
+    //createAccRect.setOutlineThickness(2);
+    //createAccRect.setOutlineColor({98, 115, 255});
+    createAccRect.setPosition(455,525);
+
+    // text for create account button
+    sf::Text createAccText;
+    createAccText.setFont(textFont);
+    createAccText.setFillColor({98, 115, 255});
+    createAccText.setCharacterSize(20);
+    createAccText.setString("Create Account");
+    createAccText.setPosition(455, 525);
 
     window->clear({223, 226, 255});
     window->draw(title);
+    window->draw(createAccText);
+    window->draw(textBoxes["Username"]->getRect());
+    window->draw(textBoxes["Password"]->getRect());
     window->display();
 }
 
-void Application::loadTextures(){
-    font.loadFromFile("../fonts/Open_Sans/static/OpenSans-ExtraBold.ttf");
+void Application::loadItems(){
+    // load fonts from folder
+    titleFont.loadFromFile("../fonts/Open_Sans/static/OpenSans-ExtraBold.ttf");
+    textFont.loadFromFile("../fonts/Open_Sans/static/OpenSans-Regular.ttf");
+
+    // create text box for entering username
+    TextBox* usernameBox = new TextBox;
+    usernameBox->setPosition(455, 400);
+    textBoxes["Username"] = usernameBox;    // load into map for easy access
+
+    // create text box for entering password
+    TextBox* passwordBox = new TextBox;
+    passwordBox->setPosition(455, 450);
+    textBoxes["Password"] = passwordBox;    // load into map for easy access
 }
 
 void Application::run(){
-    loadTextures();
+    loadItems();
+    float mouseX;
+    float mouseY;
 
     while(window->isOpen()){
         sf::Event event;
@@ -28,11 +62,61 @@ void Application::run(){
                 window->close();
                 return;
             }
-            updateLoginWindow();
+
+            if(applicationState == 0){
+
+                if(event.type == sf::Event::MouseButtonReleased){
+                    mouseX = sf::Mouse::getPosition(*window).x;
+                    mouseY = sf::Mouse::getPosition(*window).y;
+
+                    for(auto iter : textBoxes){
+                        if(iter.second->getRect().getGlobalBounds().contains(mouseX, mouseY)){
+                            for(auto iter2 : textBoxes){
+                                iter2.second->selected = false;
+                                iter2.second->setColor(sf::Color::White);
+                            }
+                            iter.second->selected = true;
+                            iter.second->setColor({240, 240, 255});
+                        }
+                        else{
+                            //iter.second.getRect().setFillColor(sf::Color::White);
+
+                        }
+                    }
+                }
+
+
+                renderLoginWindow();
+            }
+            else if(applicationState == 1){
+                // TODO: Add main application functionality
+            }
+            else if(applicationState == 2){
+                // TODO: Add account creation functionality
+            }
         }
     }
 }
 
 Application::Application(){
+    // create the application window
     window = new sf::RenderWindow(sf::VideoMode(1050, 600), "Campus Central Food");
+}
+
+sf::RectangleShape TextBox::getRect(){
+    return rect;
+}
+
+void TextBox::setPosition(float x, float y){
+    rect.setPosition({x, y});
+}
+
+void TextBox::setColor(sf::Color color){
+    this->rect.setFillColor(color);
+}
+
+TextBox::TextBox(){
+    rect.setSize({140, 25});
+    rect.setOutlineColor(sf::Color::Black);
+    rect.setOutlineThickness(2);
 }
--- application.h
+++ application.h
@@ -2,23 +2,52 @@
 #include <SFML/Graphics.hpp>
 #include <map>
 
-class Application {
+class TextBox{
 private:
-    // login window
-    sf::RenderWindow* window;
-    sf::Font font;
-    std::map<std::string, sf::Sprite>* textures;
+    // text variable and rectangle object
+    std::string boxText = "";
+    sf::RectangleShape rect;
 
 public:
-    // main driver
+    // button selected boolean
+    bool selected = false;
+
+    // setter
+    void setPosition(float x, float y);
+    void setColor(sf::Color color);
+
+    // getter
+    sf::RectangleShape getRect();
+
+    // default constructor
+    TextBox();
+};
+
+
+class Application {
+private:
+    // fonts
+    sf::Font titleFont;
+    sf::Font textFont;
+
+    // application state: 0 for login screen, 1 for main application, 2 for create account
+    int applicationState = 0;
+    sf::RenderWindow* window;
+    std::map<std::string, sf::Sprite>* textures;
+    std::map<std::string, TextBox*> textBoxes;
+
+public:
+    // main driver prototype
     void run();
 
-    // helper function for updating loginWindow
-    void updateLoginWindow();
-    void loadTextures();
+    // helper function prototypes for updating loginWindow
+    void renderLoginWindow();
+    void loadItems();
 
     // default constructor
     Application();
 };
+
+
 
 
